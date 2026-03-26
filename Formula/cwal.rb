512b81416ef039bab6e4dd633d4bc988f7c0ffba
nitinbhat972 <nitinbhat972@gmail.com>
2026-03-26 12:53:14 +0530

    bump version v0.8.2

--- Formula/cwal.rb
+++ Formula/cwal.rb
@@ -1,8 +1,8 @@
 class Cwal < Formula
   desc "Blazing-fast pywal-like color palette generator written in C"
   homepage "https://github.com/nitinbhat972/cwal"
-  url "https://github.com/nitinbhat972/cwal/archive/refs/tags/v0.8.1.tar.gz"
-  sha256 "7134c6da51cebc73dbb751f4cf2a112ccecb4940677277e9be28845d85289865"
+  url "https://github.com/nitinbhat972/cwal/archive/refs/tags/v0.8.2.tar.gz"
+  sha256 "08312d2fa120404ed0ebab9a14c934e8f076abe9eeaa862b420e6d22f69b3aae"
   license "GPL-3.0-or-later"
 
   depends_on "cmake" => :build
