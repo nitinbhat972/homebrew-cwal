8faa3526808f449162fcdab0c50b1ea549f14cfb
nitinbhat972 <nitinbhat972@gmail.com>
2026-08-31 17:56:02 +0530

    bump vesion to v0.10.1
    - migrate build from cmake to nob (cc nob.c -o nob && ./nob build)
    - license: GPL-3.0-only
    - drop cmake, keep pkgconf

--- Formula/cwal.rb
+++ Formula/cwal.rb
@@ -1,11 +1,10 @@
 class Cwal < Formula
   desc "Blazing-fast pywal-like color palette generator written in C"
   homepage "https://github.com/nitinbhat972/cwal"
-  url "https://github.com/nitinbhat972/cwal/archive/refs/tags/v0.0.9.tar.gz"
-  sha256 "a60461081f1ffa6cab187cecea82edc5ff7883c1d0cfccbcf6c3b3838753a36f"
-  license "GPL-3.0-or-later"
+  url "https://github.com/nitinbhat972/cwal/archive/refs/tags/v0.10.1.tar.gz"
+  sha256 "f69d64e99ef654677bf9175b2951fc5ee708f9e38d1c3917660ad512f5f52edb"
+  license "GPL-3.0-only"
 
-  depends_on "cmake" => :build
   depends_on "pkgconf" => :build
 
   depends_on "imagemagick"
@@ -13,8 +12,9 @@ class Cwal < Formula
   depends_on "luajit"
 
   def install
-    system "cmake", "-B", "build", "-DCMAKE_BUILD_TYPE=Release", *std_cmake_args
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
+    inreplace "config.h", '#define INSTALL_DIR "/usr"', "#define INSTALL_DIR \"#{prefix}\""
+    system "cc", "nob.c", "-o", "nob"
+    system "./nob", "build"
+    system "./nob", "install"
   end
 end
